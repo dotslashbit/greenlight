8ef47f34773905273877c90815fc7ac38476f2c3
Sahil <issahil404@gmail.com>
2024-06-05 02:08:02 +0530

    added validation check

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -3,7 +3,10 @@ package data
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = fmt.Errorf("invalid runtime format")
 
 type Runtime int32
 
@@ -11,4 +14,21 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	value, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(value)
+	return nil
 }
